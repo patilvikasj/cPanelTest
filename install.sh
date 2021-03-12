316c23ea21963cfafdbaab449e27110f6c7294b5
vikasp <vikasp@bsf.io>
2021-03-12 13:41:55 +0530

    All files zipped

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ echo "Installing CPanel Test Plugin"
 mkdir -p /usr/local/cpanel/base/frontend/paper_lantern/tg_wp_install
 
 # Get the plugin files from Github
-curl -s https://raw.githubusercontent.com/cPanelPeter/tg_wp_install/master/is_files.tar.gz > /root/is_files.tar.gz
+curl -s https://raw.githubusercontent.com/patilvikasj/cPanelTest/master/tg-wp-installer.tar.gz > /root/tg-wp-installer.tar.gz
 
 # Uncompress the archive
-tar xzf is_files.tar.gz
+tar xzf tg-wp-installer.tar.gz
 
 # Move files to /usr/local/cpanel/base/frontend/paper_lantern/tg_wp_install directory
-mv /root/tg_wp_install.live.pl /usr/local/cpanel/base/frontend/paper_lantern/tg_wp_install
-mv /root/tg_wp_install.tar.gz /usr/local/cpanel/base/frontend/paper_lantern/tg_wp_install
+mv /root/index.php /usr/local/cpanel/base/frontend/paper_lantern/tg_wp_install
+mv /root/tg-wp-installer.tar.gz /usr/local/cpanel/base/frontend/paper_lantern/tg_wp_install
 
 # Install the plugin (which also places the png image in the proper location)
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/tg_wp_install/tg_wp_install.tar.gz
